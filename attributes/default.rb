d4eb4a7c50632e3cd264cb3d28db8c81ca0fe9af
Abhaya Shenoy <abhayashenoy@gmail.com>
2013-03-01 18:07:15 +0800

    Change default deploy user and group names

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['postgresql']['password']['postgres']  = 'password'
 default['chef-rack_stack']['rails']['environment'] = 'production'
-default['chef-rack_stack']['deploy']['user'] = 'neo_deploy'
-default['chef-rack_stack']['deploy']['group'] = 'neo_deploy'
+default['chef-rack_stack']['deploy']['user'] = 'stacko'
+default['chef-rack_stack']['deploy']['group'] = 'stacko'
