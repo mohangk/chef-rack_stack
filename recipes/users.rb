8a788495dea16fe70cd7818fd0b6ca093cc7ed08
Abhaya Shenoy <abhayashenoy@gmail.com>
2013-03-01 18:07:57 +0800

    Install ssh key specific in configuration

--- recipes/users.rb
+++ recipes/users.rb
@@ -1,5 +1,6 @@
 deploy_user  = node['chef-rack_stack']['deploy']['user']
 deploy_group = node['chef-rack_stack']['deploy']['group']
+deploy_ssh_key = node['chef-rack_stack']['deploy']['ssh_key']
 
 ohai "reload_passwd" do
   action :nothing
@@ -9,7 +10,7 @@ end
 user_account deploy_user do
   comment "Deploy user"
   home "/home/#{deploy_user}"
-  #ssh_keys node['rack_stack']['deploy_user_authorized_key']
+  ssh_keys deploy_ssh_key
   notifies :reload, resources(:ohai => 'reload_passwd'), :immediately
 end
 
